5f17d6627ee222fd706ae7eedac2511c909911ce
Hebertt Nascimento <heberttaferreira.com@gmail.com>
2022-10-20 17:26:09 -0300

    requirement 2 done

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+// será avaliado se o arquivo teste About.test.js contempla 100% dos casos
+// de uso criados pelo Stryker:
+// É exibido na tela um h2 com texto About Pokédex
+// O atributo src da imagem é
+import { screen, render } from '@testing-library/react';
+import React from 'react';
+import About from '../pages/About';
+// import renderWithRouter from '../renderWithRouter';
+
+test('verifica se é exibido na tela um h2 com texto About Pokédex O atributo src da imagem é', () => {
+  render(<About />);
+  const textTitle = screen.getByText('About Pokédex');
+  const pokedex = screen.getByRole('img');
+  expect(textTitle).toBeInTheDocument();
+  expect(pokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,3 @@
-test('', () => {});
+test('testando se ao inserir um caminho incorreto é direcionado para a página notfound', () => {
+
+});
