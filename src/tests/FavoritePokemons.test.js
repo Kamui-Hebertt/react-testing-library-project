4c27ef3b8a1f2e917713088023fe390e15b089fa
Hebertt Nascimento <heberttaferreira.com@gmail.com>
2022-10-20 17:48:01 -0300

    Requirement 3 done

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+test('teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha pokémons favoritos;', () => {
+  render(<FavoritePokemons />);
+  const noFavoritesText = screen.getByText('No favorite pokemon found');
+  expect(noFavoritesText).toBeInTheDocument();
+});
