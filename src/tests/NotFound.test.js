62a5defd7e0396e96cdcd44b23a26975d62c5836
Hebertt Nascimento <heberttaferreira.com@gmail.com>
2022-10-20 18:11:17 -0300

    Requirement 4 done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,3 +1,11 @@
-test('testando se ao inserir um caminho incorreto é direcionado para a página notfound', () => {
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
 
+test('testando se ao inserir um caminho incorreto é direcionado para a página notfound e se possui uma imagem', () => {
+  render(<NotFound />);
+  const titleMessage = screen.getByRole('heading', { level: 2 });
+  expect(titleMessage).toHaveTextContent('Page requested not found');
+  const NotFoundImg = screen.getByRole('img');
+  expect(NotFoundImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
 });
